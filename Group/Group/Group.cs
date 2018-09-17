00687475d1583658f22d7ec60bcb17ccd113efdc
Akim <akim.lyubchenko@gmail.com>
2018-09-17 13:03:56 +0300

    Hometask: Group and students

---
+++ Group/Group/Group.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group
+{
+    public class Group
+    {
+        public int Number { get; private set; }
+        public List<Student.Student> students = new List<Student.Student>();
+
+        public Group(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("The group number must be greater than 0"); 
+            }
+
+            Number = number;
+        }
+        
+        public Group(int number, params Student.Student[] students) : this(number)
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i] != null)
+                {
+                    this.students.Add(students[i]);
+                }
+            }
+        }
+
+        public double AverageScore()
+        {
+            double avScore = 0;
+            int count = 0;
+
+            foreach (var student in students)
+            {
+                avScore += student.AverageScore();
+                count++;
+            }
+
+            return avScore / count;
+        }
+
+        public void AddStudent(Student.Student student)
+        {
+            students.Add(student);
+        }
+    }
+}
---
+++ Group/Student/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class Student
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        private List<int> notes = new List<int>();
+
+        public Student(string firstName, string lastName)
+        {
+            if (string.IsNullOrEmpty(firstName))
+            {
+                throw new ArgumentNullException($"Invalid name {firstName}");
+            }
+
+            if (string.IsNullOrEmpty(lastName))
+            {
+                throw new ArgumentNullException($"Invalid name {lastName}");
+            }
+
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Student(string firstName, string lastName, params int[] notes) :this(firstName, lastName)
+        {
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (notes[i] < 1 && notes[i] > 10)
+                {
+                    throw new ArgumentException($"Invalid note {notes[i]}");
+                }
+
+                this.notes.Add(notes[i]);
+            }
+        }
+
+        public double AverageScore()
+            => notes.Average();
+
+        public void AddNote(int note)
+            => notes.Add(note);
+    }
+}
